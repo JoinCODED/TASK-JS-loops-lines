f41f75d6ff566dd5a7bc080a32b3fb9021c5996d
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2021-12-26 08:04:36 +0300

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,81 @@
+/**
+ * 
+ * In this function, you will have to draw a vertical line with `n` length
+ * for example `drawVLine(5) should print out 
+  # 
+  # 
+  # 
+  # 
+  # 
+ */
+function drawVLine(n) {
+  // Continue the code here
+}
+/**
+ *
+ * In this function, you will have to draw a horizontal line with `n` length
+ * for example `drawHLine(5) should print out # # # # # (there are spaces here!)
+ */
+function drawHLine(n) {
+  // Continue the code here
+}
+
+/**
+ * 
+ * In this function, you will have to draw a square with a length of `n` 
+ * for example `drawSquare(5) should print out 
+ 
+  #  #  #  #  #
+  #  #  #  #  #
+  #  #  #  #  #
+  #  #  #  #  #
+  #  #  #  #  #
+
+ */
+function drawSquare(n) {
+  // Continue the code here
+}
+
+/**
+ * 
+ * In this function, you will have to draw a triangle with a length of `n` 
+ * for example `drawLeftTriangle(5) should print out 
+ 
+  #  
+  #  #  
+  #  #  #  
+  #  #  #  #  
+  #  #  #  #  #
+
+ */
+function drawLeftTriangle(n) {
+  // Continue the code here
+}
+
+/**
+ * 
+ * In this function, you will have to draw a tree with a length of `n` 
+ * for example `drawTree(5) should print out 
+ 
+     #   
+    # #   
+   # # # 
+  # # # # 
+     #
+
+ */
+function drawTree(n) {
+  // Continue the code here
+}
+/**
+ *
+ * In this example, you will create 4 arrays, `yellowCards`, `greenCards`, `redCards`, `blueCards`
+ * (BONUS) Try to optimize the code with doing it with one gigantic loop
+ */
+function fillUnoDeck() {
+  let greenCards = [];
+  let yellowCards = [];
+  let redCards = [];
+  let blueCards = [];
+  // Fill the cards here
+}
